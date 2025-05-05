e2afc44793450604c8b5babef65fddd2b10b5d8e
Ahmedabdelrahman22 <noaaclibly@gmail.com>
2025-05-06 01:06:32 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            // https://linked-posts.routemisr.com/uploads/default-profile.png
             protocol: 'https',
             hostname: 'linked-posts.routemisr.com',
             pathname: '/uploads/**',
